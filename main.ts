6abe8550fa70135b431d72fb773033e5db2130d5
Jasper Precht <72135430+jasperp92@users.noreply.github.com>
2023-06-14 14:23:06 +0200

    initial commit

--- main.ts
+++ main.ts
@@ -1,3 +1,57 @@
-basic.forever(function () {
-	
-})
+enum Motor {
+    A,
+    B,
+    //% block="A and B"
+    AB
+};
+
+enum MotorDirection {
+    //% block="forward"
+    Forward,
+    //% block="backward"   
+    Reverse
+};
+
+/**
+* Blocks to control the onboard motors
+*/
+//% color=#008272 weight=30 icon="\uf1b9"
+namespace motors_neu {
+
+    /**
+    * Controls one or two motors attached to the board.
+    */
+    //% blockId=block_control_motor block="motor %motor |moving %direction |at %duty_percent \\%"
+    //% duty_percent.shadow="speedPicker"
+    //% weight=80
+    //% duty_percent.defl=100
+    //% direction.defl = MotorDirection.Forward
+    export function dualMotorPower(motor: Motor, direction: MotorDirection, duty_percent: number) {
+        pins.digitalWritePin(DigitalPin.M_MODE, 1);
+        let power: number = Math.map(duty_percent, 0, 100, 0, 1023);
+        switch (motor) {
+            case Motor.A:
+                if (direction === MotorDirection.Forward) {
+                    pins.digitalWritePin(DigitalPin.M_AIN1, 0)
+                } else {
+                    pins.digitalWritePin(DigitalPin.M_AIN1, 1)
+                };
+                pins.analogWritePin(AnalogPin.M_AIN2, power)
+                break;
+            case Motor.B:
+                if (direction === MotorDirection.Forward) {
+                    pins.digitalWritePin(DigitalPin.M_BIN1, 0)
+                } else {
+                    pins.digitalWritePin(DigitalPin.M_BIN1, 1)
+                };
+                pins.analogWritePin(AnalogPin.M_BIN2, power)
+            case Motor.AB:
+                if (direction === MotorDirection.Forward) {
+                } else {
+                };
+                pins.analogWritePin(AnalogPin.P1, power)
+                pins.analogWritePin(AnalogPin.P1, power)
+                break;
+        }
+    }
+}
