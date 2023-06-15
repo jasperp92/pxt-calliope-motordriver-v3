1a2da66c68c1c4ca9095f42bbc1814240379a531
Jasper Precht <72135430+jasperp92@users.noreply.github.com>
2023-06-15 14:09:05 +0200

    Aktualisiere test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,67 @@
-// Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
+input.onPinPressed(TouchPin.P0, function () {
+    if (forward) {
+        forward = false
+    } else {
+        forward = true
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (start_Motor_A) {
+        start_Motor_A = false
+    } else {
+        start_Motor_A = true
+    }
+})
+input.onPinPressed(TouchPin.P2, function () {
+
+})
+input.onButtonPressed(Button.B, function () {
+    if (start_Motor_B) {
+        start_Motor_B = false
+    } else {
+        start_Motor_B = true
+    }
+})
+let start_Motor_B = false
+let start_Motor_A = false
+let forward = false
+forward = true
+basic.forever(function () {
+    if (forward) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        if (start_Motor_A) {
+            motors_v3.dualMotorPower(Motor.A, MotorDirection.Forward, 50)
+        } else {
+            motors_v3.brakeMotor(Motor.A)
+        }
+        if (start_Motor_B) {
+            motors_v3.dualMotorPower(Motor.B, MotorDirection.Forward, 50)
+        } else {
+            motors_v3.brakeMotor(Motor.B)
+        }
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        if (start_Motor_A) {
+            motors_v3.dualMotorPower(Motor.A, MotorDirection.Reverse, 50)
+        } else {
+            motors_v3.brakeMotor(Motor.A)
+        }
+        if (start_Motor_B) {
+            motors_v3.dualMotorPower(Motor.B, MotorDirection.Reverse, 50)
+        } else {
+            motors_v3.brakeMotor(Motor.B)
+        }
+    }
+})
